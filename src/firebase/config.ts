5afe1c6847a711ae1f2bea40828af17d29ff8cc5
Neeraj kumar <neerajkumar.muzf@gmail.com>
2025-10-10 13:29:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8735949378-fc795",
+  "appId": "1:330523146391:web:dacea548ce70d6d5de11d2",
+  "apiKey": "AIzaSyCr9Z8GocVmsFO-gLfcbB9aszKFtcWzQHc",
+  "authDomain": "studio-8735949378-fc795.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "330523146391"
+};
